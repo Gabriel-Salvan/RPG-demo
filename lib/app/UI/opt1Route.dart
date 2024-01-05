8e37522dd6e6aab235d40bc29e91f6abfbccc123
Gabriel-Salvan <gabriel.santos.salvan@gmail.com>
2024-01-04 23:28:57 -0300

    Opt1e2 Routes initial desing commit

--- lib/app/UI/opt1Route.dart
+++ lib/app/UI/opt1Route.dart
@@ -1,14 +1,99 @@
-// ignore_for_file: prefer_const_constructors
+
 
 import 'package:flutter/material.dart';
 
-class Opt1Route extends StatelessWidget{
+class Opt1Route extends StatefulWidget{
   const Opt1Route({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Opt1Route> {
+  String textToShow = "";
+  String fullText = "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla";
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animateText();
+  }
+
+  _animateText() {
+    if (currentIndex < fullText.length) {
+      setState(() {
+        textToShow += fullText[currentIndex];
+        currentIndex++;
+      });
+
+      Future.delayed(const Duration(milliseconds: 50), () {
+        _animateText();
+      });
+    }
+  }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
-    );
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        body: Column(
+            children: ([
+          Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.2,
+                  left: MediaQuery.of(context).size.width * 0.45,
+                  right: MediaQuery.of(context).size.width * 0.2),
+              child: Image.asset(
+                'lib/assets/example.png',
+                height: MediaQuery.of(context).size.height * 0.45,
+                width: MediaQuery.of(context).size.width * 0.25,
+              )),
+          Padding(
+              padding: EdgeInsets.only(
+                right: MediaQuery.of(context).size.width * 0.2,
+                left: MediaQuery.of(context).size.width * 0.2,
+                bottom: MediaQuery.of(context).size.height * 0.05,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                    MediaQuery.of(context).size.width * 0.02),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 56, 55, 55),
+                        width: MediaQuery.of(context).size.width * 0.01),
+                    borderRadius: BorderRadius.circular(
+                        MediaQuery.of(context).size.width * 0.02),
+                  ),
+                  child: Row(
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          textToShow,
+                          style:TextStyle(fontSize:MediaQuery.of(context).size.width*0.02) )
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const Opt1Route(),
+                                  ));
+                            },
+                            child: Container(
+                                color: const Color.fromARGB(255, 52, 73, 82),
+                                child: const Text('Option 1'))),
+                      )
+                    ],
+                  ),
+                ),
+              )),
+        ])));
   }
 }
--- lib/app/UI/opt2Route.dart
+++ lib/app/UI/opt2Route.dart
@@ -1,13 +1,99 @@
 import 'package:flutter/material.dart';
 
-class Opt2Route extends StatelessWidget {
+class Opt2Route extends StatefulWidget {
   const Opt2Route({super.key});
 
 
 @override
-Widget build(BuildContext context) {
-  return const Scaffold(
-    body: Column(),
-  );
+  _MyHomePageState createState() => _MyHomePageState();
 }
+
+class _MyHomePageState extends State<Opt2Route> {
+  String textToShow = "";
+  String fullText =
+      "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla";
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animateText();
+  }
+
+  _animateText() {
+    if (currentIndex < fullText.length) {
+      setState(() {
+        textToShow += fullText[currentIndex];
+        currentIndex++;
+      });
+
+      Future.delayed(const Duration(milliseconds: 50), () {
+        _animateText();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        body: Column(
+            children: ([
+          Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.2,
+                  left: MediaQuery.of(context).size.width * 0.45,
+                  right: MediaQuery.of(context).size.width * 0.2),
+              child: Image.asset(
+                'lib/assets/example.png',
+                height: MediaQuery.of(context).size.height * 0.45,
+                width: MediaQuery.of(context).size.width * 0.25,
+              )),
+          Padding(
+              padding: EdgeInsets.only(
+                right: MediaQuery.of(context).size.width * 0.2,
+                left: MediaQuery.of(context).size.width * 0.2,
+                bottom: MediaQuery.of(context).size.height * 0.05,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                    MediaQuery.of(context).size.width * 0.02),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 56, 55, 55),
+                        width: MediaQuery.of(context).size.width * 0.01),
+                    borderRadius: BorderRadius.circular(
+                        MediaQuery.of(context).size.width * 0.02),
+                  ),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.topRight,
+                          child: Text(textToShow,
+                              style: TextStyle(
+                                  fontSize: MediaQuery.of(context).size.width *
+                                      0.02))),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const Opt2Route(),
+                                  ));
+                            },
+                            child: Container(
+                                color: const Color.fromARGB(255, 52, 73, 82),
+                                child: const Text('Option 1'))),
+                      )
+                    ],
+                  ),
+                ),
+              )),
+        ])));
+  }
 }

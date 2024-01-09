a47d95955913214276985f74a22aebd1f69946af
Gabriel-Salvan <gabriel.santos.salvan@gmail.com>
2024-01-09 20:27:58 -0300

    DialogScreenRoute2 demo commit

---
+++ lib/app/UI/dialogScreenRoute2.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class DialogScreenRoute1 extends StatelessWidget {
+  const DialogScreenRoute1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        body: Column(
+            children: ([
+          Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.2,
+                  left: MediaQuery.of(context).size.width * 0.45,
+                  right: MediaQuery.of(context).size.width * 0.2),
+              child: Image.asset(
+                'lib/assets/images/example.png',
+                height: MediaQuery.of(context).size.height * 0.45,
+                width: MediaQuery.of(context).size.width * 0.25,
+              )),
+          Padding(
+              padding: EdgeInsets.only(
+                right: MediaQuery.of(context).size.width * 0.2,
+                left: MediaQuery.of(context).size.width * 0.2,
+                bottom: MediaQuery.of(context).size.height * 0.05,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                    MediaQuery.of(context).size.width * 0.02),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 56, 55, 55),
+                        width: MediaQuery.of(context).size.width * 0.01),
+                    borderRadius: BorderRadius.circular(
+                        MediaQuery.of(context).size.width * 0.02),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Opt5Route(),
+                                ));
+                          },
+                          child: Container(
+                              color: const Color.fromARGB(255, 52, 73, 82),
+                              child: const Text('Option 1'))),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Opt6Route(),
+                                ));
+                          },
+                          child: Container(
+                            color: const Color.fromARGB(255, 52, 73, 82),
+                            child: const Text('Option 2'),
+                          )),
+                    ],
+                  ),
+                ),
+              )),
+        ])));
+  }
+}

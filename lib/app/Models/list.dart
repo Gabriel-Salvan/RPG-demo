7d73163b8f264236c269aca97bb1a167358d3013
Gabriel-Salvan <gabriel.santos.salvan@gmail.com>
2024-01-17 21:19:47 -0300

    Route 1 e 2 commi[t #vlwJedhai

--- lib/app/Models/list.dart
+++ lib/app/Models/list.dart
@@ -11,14 +11,33 @@ Map<int, Page> routes = {
       routeToGo: 3),
   3: ButtonPage(
       button1: ButtonToNavigate(buttonText: "Nossaaaaa!!!, um burro. Cê tá beeem??? Tá machucado???!", routeToGo: 4),
-      button2: ButtonToNavigate(buttonText: "Que nojo!!! Sebo da peeeste! Certeza que tem raiva esse bixo! Sai da minha frente carambaaaa!", routeToGo: 5),
+      button2: ButtonToNavigate(buttonText: "Que nojo!!! Sebo da peeeste! Certeza que tem raiva esse bixo! Sai da minha frente carambaaaa!", routeToGo: 5,),
       imagePath: "lib/assets/images/example.png"),
   4: TextPage(
       imagePath: "lib/assets/images/example.png",
-      textToShow: "text1 text1 text1 text1 text1 text1 ",
-      routeToGo: 3),
+      textToShow: "Suas palavras aparentam ter animado ele um pouco. Ele então se levanta e pega uma foto. Parece ser de uma Burra com aparência meio diferenciada.",
+      routeToGo: 11),
   5: TextPage(
       imagePath: "lib/assets/images/example.png",
-      textToShow: "text1 text1 text1 text1 text1 text1 ",
-      routeToGo: 3),
+      textToShow: "Ao ouvir tuas palavras profanas, o burro colapsa no chão, e ao seu redor é formada uma grande poça de suas lagrimas",
+      routeToGo: 6),
+  6: ButtonPage(
+      button1: ButtonToNavigate(buttonText: "Foi mal mermão... qué ajuda??", routeToGo: 7),
+      button2: ButtonToNavigate(buttonText: "Credoooooo *Você o ignora e continua andando.", routeToGo: 8,),
+      imagePath: "lib/assets/images/example.png"),
+  8: TextPage(
+      imagePath: "lib/assets/images/example.png",
+      textToShow:
+          "Após ver seu comentário, ele continua chorando, mas com intensidade que você nunca havia visto antes.",
+      routeToGo: 9),
+  9: TextPage(
+      imagePath: "lib/assets/images/example.png",
+      textToShow:
+          "A água começa a subir, fazendo com que você se afogue em suas lagrimas.",
+      routeToGo: 6),
+  7: TextPage(
+      imagePath: "lib/assets/images/example.png",
+      textToShow:
+          "Ao ver o burro sofrer mais do que ele já estava, você se enche de culpa e tenta ajudar...",
+      routeToGo: 4),
 };
